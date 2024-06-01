f02bb48d86cb895bdad494c798895cc9653de3c9
arcenioCorteis <arcenioCorteis@github.com>
2024-06-02 01:08:12 +0200

    feat: Create a web crawler to get Image

--- a_posts/forms.py
+++ a_posts/forms.py
@@ -5,13 +5,14 @@ from django import forms
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['url', 'body']
         
         labels = {
             'body': 'Caption',
         }
         widgets = {
-            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption ...', 'class': 'font1 text-4xl'})
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption ...', 'class': 'font1 text-4xl'}),
+            'url': forms.TextInput(attrs={'placeholder': 'Add url ...'})
         }
         
     
--- a_posts/views.py
+++ a_posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from bs4 import BeautifulSoup
+import requests
 
 def home_view(request):
     posts = Post.objects.all()
@@ -12,6 +14,22 @@ def post_create_view(request):
     if request.method == 'POST':
         form = PostCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+            website = requests.get(form.data['url'])
+            source_code = BeautifulSoup(website.text, 'html.parser')
+            
+            find_image =source_code.select('meta[content^="https://live.staticflickr.com/"]')
+            image = find_image[0]['content']
+            post.image = image
+            
+            find_title = source_code.select('h1.photo-title')
+            title  = find_title[0].text.strip()
+            post.title = title
+            
+            find_artist = source_code.select('a.owner-name')
+            artist = find_artist[0].text.strip()
+            post.artist = artist
+
+            post.save()
             return redirect('home')
     return render(request, 'a_posts/post_create.html', {'form': form})
