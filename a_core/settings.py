49faf3261b9d8ea9c1c70133005a43b80e100f04
arcenioCorteis <arcenioCorteis@github.com>
2024-06-02 16:06:44 +0200

    feat: Create Tag

--- a_core/settings.py
+++ a_core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9n67^n=2d#4nxw5@45#tbof0)o89p5^zeic&$i8t=7+_9z97fj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
