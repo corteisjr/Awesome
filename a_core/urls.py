65c33a72fcc663072267f6f8e9d615908534766c
arcenioCorteis <arcenioCorteis@github.com>
2024-06-02 20:30:01 +0200

    feat: Add Icon Category and statics settings;

--- a_core/urls.py
+++ a_core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from a_posts.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,5 @@ urlpatterns = [
     path('post/edit/<pk>/', post_edit_view, name='post-edit'),
     path('post/<pk>/', post_page_view, name='post-page'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
