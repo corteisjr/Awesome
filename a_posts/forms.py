133f4097efc401016227943bc1c4f8410f262ad4
arcenioCorteis <arcenioCorteis@github.com>
2024-06-02 15:28:30 +0200

    feat: Edit post

--- a_posts/forms.py
+++ a_posts/forms.py
@@ -15,5 +15,17 @@ class PostCreateForm(ModelForm):
             'url': forms.TextInput(attrs={'placeholder': 'Add url ...'})
         }
         
+class PostEditForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['body']
+        
+        labels = {
+            'body': '',
+        }
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption ...', 'class': 'font1 text-4xl'}),
+        }
+        
     
         
